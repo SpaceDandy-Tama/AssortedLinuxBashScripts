d0b30c6f6b508de13b5d8c3939d93146eb106966
Oğuz Can Soyselçuk <35855056+SpaceDandy-Tama@users.noreply.github.com>
2023-05-22 11:26:17 +0300

    Update PepeePopos.sh

--- PepeePopos.sh
+++ PepeePopos.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-LOG_FILE="PepeePopos.Log"
-exec > >(tee ${LOG_FILE}) 2>&1
+
+#Run this script, immediately after installing Pop Os, using sudo
+
 cd /home/$USER
 
 # Let's create some useful templates
 Dir="/home/$USER/Templates"
 [ -d "$Dir" ] || mkdir "$Dir"
 touch "$Dir/Text File.txt"
-echo '#!/bin/bash' > "$Dir/Bash Script.sh"
-sudo chmod +x "$Dir/Bash Script.sh"
 
 # Show Hidden Files and Folders (Not just for nautilus)
 dumpFile="gsettings.dump"
@@ -51,23 +50,21 @@ Packages[4]=gstreamer1.0-libav # AAC decoder plugin https://gitlab.freedesktop.o
 Packages[5]=gstreamer1.0-plugins-good # H.265 decoder plugin https://gitlab.freedesktop.org/gstreamer/gstreamer
 Packages[6]=ffmpeg # H.264 decoder plugin https://git.ffmpeg.org/ffmpeg.git
 Packages[7]=7zip # 7zz https://www.7-zip.org/
-Packages[8]=notepadqq #Notepad++ Analogue https://github.com/notepadqq/notepadqq
+Packages[8]=notepadqq # Notepad++ Analogue https://github.com/notepadqq/notepadqq
 Packages[9]=audacious # Audio Player https://github.com/audacious-media-player/audacious
 Packages[10]=vlc # Video Player https://github.com/videolan/vlc
-Packages[11]=discord # Discord https://discord.com/
+Packages[11]=okular # PDF reader (has background color change support)
 Packages[12]=steam # Steam https://store.steampowered.com/
 Packages[13]=transmission # Torrent Client
 Packages[14]=gamemode # CPU Governor https://github.com/FeralInteractive/gamemode
-Packages[15]=git #git
-Packages[16]=git-lfs #git large file thingy
+Packages[15]=git # git
+Packages[16]=git-lfs # git large file thingy
 Packages[17]=rabbitvcs-nautilus # Nautilus Git integration thingy
-Packages[18]=krita #Photoshop alternative
-Packages[19]=inkscape #Illustrator alternative
-Packages[20]=kdenlive #Video Editor
-Packages[21]=obs-studio #Screen Recorder
-Packages[22]=okular # PDF reader (has background color change support)
-Packages[23]=flatseal # manage flatpack permissions
-Packages[24]=spice-vdagent # virtual machine guest tools
+Packages[18]=krita # Photoshop alternative
+Packages[19]=inkscape # Illustrator alternative
+Packages[20]=kdenlive # Video Editor
+Packages[21]=obs-studio # Screen Recorder
+Packages[22]=discord # Social
 
 for i in "${Packages[@]}"
 do
@@ -88,56 +85,12 @@ echo 'Right click and choose "Open Link", or copy the link and paste into your b
 # xdg-open https://extensions.gnome.org/extension/1852/gamemode/ #using xdg-open for internet address seems to cause a bug
 read -p "Press enter to continue..."
 
-# AppImageLauncher https://github.com/TheAssassin/AppImageLauncher
-sudo add-apt-repository ppa:appimagelauncher-team/stable -y
-sudo apt update
-if apt-cache show appimagelauncher; then
-	sudo apt install appimagelauncher -y
-else
-	echo "appimagelauncher for this distroy wasnt found, removing ppa."
-	sudo add-apt-repository ppa:appimagelauncher-team/stable --remove -y
-fi
-
-# Spotify
-if apt-cache show curl; then
-	sudo apt install curl -y
-	curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
-	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt update
-	sudo apt install spotify-client -y
-fi
-
-# Opera Browser
-declare -a OperaDependencies=(software-properties-common apt-transport-https wget ca-certificates gnupg2 ubuntu-keyring)
-OperaDependenciesInstalled=true
-for i in "${OperaDependencies[@]}"
-do
-	if apt-cache show $i; then
-		sudo apt install $i -y
-	else
-		SimpleDependenciesInstalled = false
-		echo "Skipping $i installation..."
-	fi
-done
-if [ "$OperaDependenciesInstalled" = true ] ; then
-	wget -O- https://deb.opera.com/archive.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/opera.gpg
-	echo deb [arch=amd64 signed-by=/usr/share/keyrings/opera.gpg] https://deb.opera.com/opera-stable/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list
-	sudo apt update
-	if sudo apt install opera-stable -y; then
-		# Opera Browser ffmpeg fix (https://github.com/Ld-Hagen/fix-opera-linux-ffmpeg-widevine)
-		git clone https://github.com/Ld-Hagen/fix-opera-linux-ffmpeg-widevine.git
-		cd ./fix-opera-linux-ffmpeg-widevine
-		sudo ./scripts/fix-opera.sh
-		sudo ./install.sh
-		cd .. && rm -rf ./fix-opera-linux-ffmpeg-widevine
-	fi
-fi
-
-# Webp support to Image Viewer
-sudo add-apt-repository ppa:helkaluin/webp-pixbuf-loader
-sudo apt update
+# Webp support to Image Viewer (ppa:helkaluin/webp-pixbuf-loader is only needed pre 22.04)
+#sudo add-apt-repository ppa:helkaluin/webp-pixbuf-loader
+#sudo apt update
 sudo apt install webp-pixbuf-loader
 
 # Cleanup
+sudo apt purge kdeconnect -y
 sudo apt autoremove -y
 sudo apt clean
